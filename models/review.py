807e298b0a4c81b1d213825078d05a4739dc1710
kmishmael <kmishmael@gmail.com>
2023-08-10 01:10:29 +0300

    add Review Model

---
+++ models/review.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Review model class module"""
+
+
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """
+    Review class that inherits from BaseModel.
+    
+    Public class attributes:
+        place_id {string} - empty string
+        user_id {string} - empty string
+        text {string} - empty string
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
