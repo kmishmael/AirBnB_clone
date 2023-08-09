afb7126923d4f537c6520dbf4da361084972fb6e
kmishmael <kmishmael@gmail.com>
2023-08-10 01:10:55 +0300

    add State Model

---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""State model class module"""
+
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """
+    State class that inherits from BaseModel.
+    
+    Public class attributes:
+        name {string} - empty string
+    """
+    name = ""
