547edbce2c70a5058d2e88ccac5929f52b155a7a
kmishmael <kmishmael@gmail.com>
2023-08-10 01:00:31 +0300

    add more unittests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python3
+
 import unittest
 from models.base_model import BaseModel
 from datetime import datetime
+import time
 
 class TestBaseModel(unittest.TestCase):
     """Test cases for the BaseModel class
@@ -10,9 +13,15 @@ class TestBaseModel(unittest.TestCase):
         """Test that docstrings are defined
         """
         self.assertIsNotNone(BaseModel.__doc__)
+        self.assertIsNotNone(BaseModel.__init__.__doc__)
         self.assertIsNotNone(BaseModel.__str__.__doc__)
         self.assertIsNotNone(BaseModel.save.__doc__)
         self.assertIsNotNone(BaseModel.to_dict.__doc__)
+
+    def test_instantiation(self):
+        """Test if an instance is created"""
+        model = BaseModel()
+        self.assertEqual(BaseModel, type(model))
 
     def test_valid_uuid_string(self):
         """Test if uuid is a string
@@ -21,33 +30,67 @@ class TestBaseModel(unittest.TestCase):
         self.assertTrue(isinstance(model.id, str))
         self.assertEqual(len(model.id), 36)
     
-    def test_valid_datetime(self):
+    def test_created_at_valid_datetime(self):
+        """Test if created_at is a valid datetime
+        """
         model = BaseModel()
         self.assertTrue(isinstance(model.created_at, datetime))
+
+    def test_updated_at_valid_datetime(self):
+        """Test if updated_at is a valid datetime
+        """
+        model = BaseModel()
         self.assertTrue(isinstance(model.updated_at, datetime))
 
-    def test_uuid_not_equal(self):
+    def test_model_uuid_unique(self):
+        """Test that uuid generated is `unique`
+        """
         model1 = BaseModel()
         model2 = BaseModel()
         self.assertNotEqual(model1.id, model2.id)
 
+    def test_model_created_at(self):
+        """Test that two models created at different times are different
+        """
+        model1 = BaseModel()
+        time.sleep(0.1)
+        model2 = BaseModel()
+        self.assertLess(model1.created_at, model2.created_at)
+
+    def test_models_updated_at(self):
+        """Test that the two models updated at is different at
+        initialization.
+        """
+        model1 = BaseModel()
+        time.sleep(0.1)
+        model2 = BaseModel()
+        self.assertLess(model1.updated_at, model2.updated_at)
+        
     def test_str_representation(self):
+        """Test that the str representation is correct
+        """
         model = BaseModel()
         self.assertIn(model.__class__.__name__, str(model))
         self.assertIn(model.id, str(model))
+        self.assertIn(str(model.__dict__), str(model))
+        self.assertEqual(f"[{model.__class__.__name__}] ({model.id}) {model.__dict__}]", str(model))
 
     def test_save_updates_updated_at(self):
+        """Test if saving updates updated_at
+        """
         model = BaseModel()
         old_updated_at = model.updated_at
         model.save()
         self.assertNotEqual(old_updated_at, model.updated_at)
 
     def test_to_dict_returns_dict(self):
+        """Test the `to_dict()` actually returns a <class dict>"""
         model = BaseModel()
         obj_dict = model.to_dict()
         self.assertTrue(isinstance(obj_dict, dict))
 
     def test_to_dict_contains_expected_keys(self):
+        """Test that the dictionary contains expected keys"""
         model = BaseModel()
         expected_keys = ['id', 'created_at', 'updated_at', '__class__']
         obj_dict = model.to_dict()
---
+++ tests/test_models/test_user.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+"""UnitTests for User Model"""
+
+
+import unittest
+from models.user import User
+from time import sleep
+from datetime import datetime
+from models import storage
+
+class TestUserModel(unittest.TestCase):
+    """Unit Tests for User Model"""
+
+    def test_no_args(self):
+        user = User()
+        self.assertEqual(User, type(user))
+    
+    def test_id_is_public_str(self):
+        user = User()
+        self.assertEqual(str, type(user.id))
+
+    def test_created_at_is_public_datetime(self):
+        user = User()
+        self.assertEqual(str, type(user.created_at))
+
+    def test_updated_at_is_public_datetime(self):
+        user = User()
+        self.assertEqual(str, type(user.updated_at))
+
+    def test_email_is_public_str(self):
+        user = User()
+        self.assertEqual(str, type(user.email))
+
+    def test_password_is_public_str(self):
+        user = User()
+        self.assertEqual(str, type(user.password))
+
+    def test_first_name_is_public_str(self):
+        user = User()
+        self.assertEqual(str, type(user.first_name))
+
+    def test_last_name_is_public_str(self):
+        user = User()
+        self.assertEqual(str, type(user.last_name))
+
+    def test_new_instance_in_file_object(self):
+        user = User()
+        self.assertIn("{}.{}".format(user.__class__.__name__, user.id), storage.all().keys)
+        self.assertIn(user, storage.all().values())
+
+    def test_user_unique_uuid(self):
+        user1 = User()
+        user2 = User()
+        self.assertNotEqual(user1.id, user2.id)
+
+    def test_different_created_at(self):
+        user1 = User()
+        sleep(0.1)
+        user2 = User()
+        self.assertNotEqual(user1.created_at, user2.created_at)
+
+    def test_different_updated_at(self):
+        user1 = User()
+        sleep(0.1)
+        user2 = User()
+        self.assertNotEqual(user1.updated_at, user2.updated_at)
+
+if __name__ == "__main__":
+    unittest.main()
