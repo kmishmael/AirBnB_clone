1880089425a6984459a02b51146cae8fe2e5fb87
kmishmael <kmishmael@gmail.com>
2023-08-10 01:09:09 +0300

    add Amenity Model

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Amenity model class module"""
+
+
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """
+    Amenity class that inherits from BaseModel.
+    
+    Public class attributes:
+        name {string} - empty string
+    """
+    name = ""
